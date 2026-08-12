535892d073f28ddea73d21137aea7f022473f6e3
gabrielinuz <gabrielinuz@gmail.com>
2026-08-12 17:03:40 -0300

    first commit

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ DB_USER=samplevault
 DB_PASS=samplevault
 DB_NAME=samplevault
 JWT_SECRET=tu_clave_secreta_super_segura
-NODE_ENV=production
+NODE_ENV=testing
 EOF
 
 echo "🚀 Configuración de entorno completada con éxito."
